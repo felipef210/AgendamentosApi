c2b4b32a471f89a043dec291113c9557e8943c93
Felipe Miranda <felipe.santos@mosten.com>
2025-10-30 16:37:40 -0300

    fix: Removendo a condicional de ambiente de desenvolvimento para o swagger

--- Program.cs
+++ Program.cs
@@ -76,12 +76,10 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
